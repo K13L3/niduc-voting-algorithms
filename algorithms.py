7e7f5352f7a33940d1fd223219d2842dd31c9437
stefan <stefan7bogdan@gmail.com>
2024-06-23 21:17:49 +0200

    Add basic functionality

--- algorithms.py
+++ algorithms.py
@@ -1,7 +1,8 @@
 import numpy as np
 import random
+import statistics
 from collections import Counter
-
+from sklearn.linear_model import LinearRegression
 
 # Generator sygnału
 class SignalGenerator:
@@ -10,7 +11,7 @@ class SignalGenerator:
         self.samples = samples  # Liczba probek w okresie
         self.periods = periods  # Liczba okresów
 
-    # Generowanie próbek sygnału f(x)=50sin+50
+    # Generowanie próbek sygnału f(x)=50sin+50 , ZWf = [0;100]
     def generate_signal(self):
         # Obliczanie częstotliwości próbkowania
         sampling_rate = self.samples / (2 * np.pi)
@@ -32,12 +33,14 @@ class Sensor:
         self.delta = delta
         self.signal = 0
         self.distorted_signal = 0
+        reliabilities = [0.95, 0.99]
+        self.reliability = random.choice(reliabilities)
 
     def read_input(self, signal):
         self.signal = signal
 
     def distort_signal(self):
-        dist = random.randint(-self.delta, self.delta)
+        dist = random.uniform(-self.delta, self.delta)
         self.distorted_signal = self.signal + dist
 
     def get_output(self):
@@ -47,7 +50,7 @@ class Sensor:
 class Voter:
     def __init__(self):
         self.readings = []
-        self.output = 0
+        self.output = 0.0
 
     def read_input(self, readings):
         self.readings = readings
@@ -57,20 +60,37 @@ class Voter:
 
     def get_output(self):
         return self.output
+        
+#Głosowanie medianowe
+class MedianVoter(Voter):  
+    def vote(self):
+        self.vote = statistics.median(self.readings)
 
+#Głosowanie metodą maksymalnego prawdopodobieństwa
+class MaximumLikelihood(Voter):
+    def vote(self):
+        a=1
+    
 
 # Głosowanie większościowe
 class MajorityVoter(Voter):
     def vote(self):
-        # Policz występowanie poszczególnych odczytów
-        reading_counts = Counter(self.readings)
-        # Znajdź najczęściej występujący odczyt
-        most_common_reading, count = reading_counts.most_common(1)[0]
-        # Jeżeli wszystkie są różne
-        if count == 1:
-            self.output = None # Brak wyniku
+        count = {}
+        # Przypisz poszczególne odczyty do klas
+        for reading in self.readings:
+            rounded = round(reading) # 1.0 2.0 3.0
+            if (count.get(rounded)):    
+                count[rounded] += 1
+            else:
+                count[rounded] = 1
+
+        # Znajdz najczesciej wystepujacy odczyt
+        max_count_key = max(count, key=count.get)
+        if(count[max_count_key] == 1):
+            self.output = None
         else:
-            self.output = most_common_reading
+            self.output = max_count_key
+
 
 
 # Głosowanie wygładzające
@@ -85,12 +105,19 @@ class SmoothingVoter(Voter):
         self.previous = previous
 
     def vote(self):
-        # Policz występowanie poszczególnych odczytów
-        reading_counts = Counter(self.readings)
-        # Znajdź najczęściej występujący odczyt
-        most_common_reading, count = reading_counts.most_common(1)[0]
+        count = {}
+        # Przypisz poszczególne odczyty do klas
+        for reading in self.readings:
+            rounded = round(reading * 2) / 2
+            if (count.get(rounded)):
+                count[rounded] += 1
+            else:
+                count[rounded] = 1
+
+        # Znajdz najczesciej wystepujacy odczyt
+        max_count_key = max(count, key=count.get)
         # Jeżeli wszystkie są różne
-        if count == 1:
+        if(count[max_count_key] == 1):
             # Wybierz odczyt najbliższy poprzedniemu
             # Oblicz różnice poszczególnych odczytów
             diffs = [abs(self.previous - reading) for reading in self.readings]
@@ -102,5 +129,45 @@ class SmoothingVoter(Voter):
             else:
                 self.output = None
         else:
-            self.output = most_common_reading
+            self.output = max_count_key
 
+#Głosowanie metodą średniej ważonej
+
+class Weighted_average_voter(Voter):
+    def  __init__(self):
+        super().__init__()        
+        self.weights = [0.3, 0.3, 0.4] #wagi poszczegolnych voterow (mozna dostosowac)
+    
+    def vote(self):
+        weighted_sum = sum([self.readings[i] * self.weights[i] for i in range(len(self.readings))])
+        total_weight = sum(self.weights)
+        self.output = weighted_sum / total_weight
+
+class Linear_Predictor_Voter(Voter):
+    def  __init__(self):
+        super().__init__() 
+        self.model = LinearRegression()
+        self.historical_data = []
+
+    def read_input(self, readings):
+        super().read_input(readings)
+        if len(self.historical_data) >= 3:
+            self.historical_data = self.historical_data[1:]  # Usuwa najstarszy odczyt, jeśli jest ich więcej niż 3
+        self.historical_data.append(readings)  # Dodaje najnowszy odczyt
+        if len(self.historical_data) > 3:
+            self.fit_linear_model() # Dostosowuje model regresji liniowej
+    
+    def fit_linear_model(self):
+        if len(self.historical_data) > 3:
+            X = np.array([data[:-1] for data in self.historical_data])
+            y = np.array([data[-1] for data in self.historical_data])
+            self.model.fit(X, y)
+
+    def vote(self):
+        if len(self.historical_data) > 3:
+            X_new = np.array(self.readings).reshape(1, -1)
+            self.output = self.model.predict(X_new)[0]
+        else:
+            self.output = sum(self.readings) / len(self.readings)
+    
+    
--- main.py
+++ main.py
@@ -3,31 +3,179 @@ import statistics
 from algorithms import *
 
 def main():
+
+    # tabelka()
+    simulate(10000, 10, 0.2, 0.4) # samples, periods, delta, threshold
+    simulate(10000, 10, 0.5, 0.4)
+    simulate(10000, 10, 0.6, 0.4)
+    simulate(10000, 10, 0.7, 0.4)
+    print("------")
+    simulate(10000, 10, 0.5, 0.2) # samples, periods, delta, threshold
+    simulate(10000, 10, 0.5, 0.3)
+    simulate(10000, 10, 0.5, 0.5)
+    simulate(10000, 10, 0.5, 0.7)
+    
+
+def tabelka():
     # Inicjalizacja instancji obiektów
     signal_generator = SignalGenerator(100, 5)
     sensor1 = Sensor(5)
     sensor2 = Sensor(5)
     sensor3 = Sensor(5)
+    
     majority_voter = MajorityVoter()
     smoothing_voter = SmoothingVoter(0.5)
+    linear_voter = Linear_Predictor_Voter()
+    weighted_average_voter = Weighted_average_voter()
 
     # Podstawowa tabelka
-    basic_signals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-    dist1_signals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Zmienic na zaburzone
-    dist2_signals = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]  # Zmienic na zaburzone
+    basic_signals = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0]
+    dist1_signals = [1.5, 1.9, 3.6, 4.4, 5.4, 6.2, 7.1, 8.3, 9.8, 10.6]  # Zmienic na zaburzone
+    dist2_signals = [1.8, 2.5, 3.3, 4.2, 5.8, 6.1, 7.9, 8.2, 9.3, 10.1]  # Zmienic na zaburzone
+    dist3_signals = [1.2, 2.3, 3.2, 4.6, 5.5, 6.9, 7.7, 8.4, 9.8, 10.4]
+
+    # Tablice z wynikami poszczególnych algorytmów
     majority_out = []
     smoothing_out = []
+    weighted_out = []
+    linear_out = []
+
     signal_generator.set_signals(basic_signals)
     for i in range(0, 10):
-        majority_voter.read_input(basic_signals)
-        majority_voter
+        readings = dist3_signals[i], dist1_signals[i], dist2_signals[i]
 
+        # Głosowanie więszkościowe
+        majority_voter.read_input(readings)
+        majority_voter.vote()
+        majority_out.append(majority_voter.get_output())
 
-    # Symulacje
+        # Głosowanie wygładzające
+        smoothing_voter.read_input(readings)
+        smoothing_voter.vote()
+        smoothing_out.append(smoothing_voter.get_output())
+
+        #Glosowanie weight
+        weighted_average_voter.read_input(readings)
+        weighted_average_voter.vote()
+        weighted_out.append(weighted_average_voter.get_output())
+
+        #Glosowanie_liniowe
+        linear_voter.read_input(readings)
+        linear_voter.vote()
+        linear_out.append(linear_voter.get_output())
+
+    print("Wiekszosciowe: ", majority_out)
+    print("Wygladzajace: ", smoothing_out)
+    print("Wazone: ", weighted_out)
+    print("Liniowe: ", linear_out)
+
+# Symuluj liczba próbek w okresie, liczba okresów
+def simulate(samples, periods, delta, threshold):
+    # Inicjalizacja instancji obiektów
+    signal_generator = SignalGenerator(samples, periods)
+    sensor1 = Sensor(delta)
+    sensor2 = Sensor(delta)
+    sensor3 = Sensor(delta)
+    
+    majority_voter = MajorityVoter()
+    smoothing_voter = SmoothingVoter(0.5)
+    linear_voter = Linear_Predictor_Voter()
+    weighted_average_voter = Weighted_average_voter()
+
+    # Tablice z wynikami poszczególnych algorytmów
+    majority_out = []
+    smoothing_out = []
+    weighted_out = []
+    linear_out = []
+
+    signal_generator.generate_signal()
+    signal = signal_generator.get_signal()
+    for sig in signal:
+        # Odczyt sygnałów
+        sensor1.read_input(sig)
+        sensor2.read_input(sig)
+        sensor3.read_input(sig)
+        # Zaburzanie sygnałów
+        sensor1.distort_signal()
+        sensor2.distort_signal()
+        sensor3.distort_signal()
+        # Tablica z odczytami
+        readings = [sensor1.get_output(),sensor2.get_output(),sensor3.get_output()]
+        # Głosowanie więszkościowe
+        majority_voter.read_input(readings)
+        majority_voter.vote()
+        majority_out.append(majority_voter.get_output())
+
+        # Głosowanie wygładzające
+        smoothing_voter.read_input(readings)
+        smoothing_voter.vote()
+        smoothing_out.append(smoothing_voter.get_output())
+
+        #Glosowanie weight
+        weighted_average_voter.read_input(readings)
+        weighted_average_voter.vote()
+        weighted_out.append(weighted_average_voter.get_output())
+
+        #Glosowanie_liniowe
+        linear_voter.read_input(readings)
+        linear_voter.vote()
+        linear_out.append(linear_voter.get_output())
+
+    majority_good = 0
+    majority_bad = 0
+    smoothing_good = 0
+    smoothing_bad = 0
+    weighted_good = 0
+    weighted_bad = 0
+    linear_good = 0
+    linear_bad = 0
+
+    for i in range(len(signal)):
+         # Głosowanie więszkościowe
+    
+        if(majority_out[i] is not None):
+            if(abs(signal[i] - majority_out[i]) <= threshold):
+                majority_good += 1
+            else:
+                majority_bad += 1
+        else:
+            majority_bad += 1
+
+        # Głosowanie wygładzające
+        if(smoothing_out[i] is not None):
+            if(abs(signal[i] - smoothing_out[i]) <= threshold):
+                smoothing_good += 1
+            else:
+                smoothing_bad += 1
+        else:
+            smoothing_bad += 1
+
+        #Glosowanie weight
+        if(abs(signal[i] - weighted_out[i]) <= threshold):
+            weighted_good += 1
+        else:
+            weighted_bad += 1
+
+        #Glosowanie_liniowe
+        if(abs(signal[i] - linear_out[i]) <= threshold):
+            linear_good += 1
+        else:
+            linear_bad += 1
+
+    n_of_readings = len(signal)
+    majority_availabilty = majority_good / n_of_readings
+    smoothing_availabilty = smoothing_good / n_of_readings
+    weighted_availabilty = (weighted_good / n_of_readings)
+    lineary_availabilty = linear_good / n_of_readings
+    
+    print("majority: ",majority_availabilty, sep=" ")
+    print("smoothing: ",smoothing_availabilty, sep=" ")
+    print("weighted: ",weighted_availabilty, sep=" ") 
+    print("linear: ",lineary_availabilty, sep=" ")
+
 
     # Dla różnego próbkowania
-    # Dla różnego wprowadzania błedów (co 10-20 cykli)
-
+    
     # Dla 100 próbek
     # 10 okresów, 100 próbek
 
